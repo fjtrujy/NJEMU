0df015f2f91f0d90b3db29e4f9b8fded27ce709a
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2026-09-19 23:35:13 +0200

    Add NCDZ simultaneous controller routing

--- src/ncdz/inptport.c
+++ src/ncdz/inptport.c
@@ -47,8 +47,12 @@ static const uint8_t hotkey_mask[11] =
 static uint8_t ALIGN16_DATA input_flag[MAX_INPUTS];
 static int ALIGN16_DATA af_map1[NCDZ_BUTTON_MAX];
 static int ALIGN16_DATA af_map2[NCDZ_BUTTON_MAX];
-static int ALIGN16_DATA af_counter[NCDZ_BUTTON_MAX];
+static int ALIGN16_DATA af_counter[2][NCDZ_BUTTON_MAX];
 static int input_ui_wait;
+
+static void update_inputport0(void);
+static void update_inputport1(void);
+static void update_inputport2(void);
 
 
 /******************************************************************************
@@ -59,9 +63,10 @@ static int input_ui_wait;
 	Update autofire flag
 ------------------------------------------------------*/
 
-static uint32_t update_autofire(uint32_t buttons)
+static uint32_t update_autofire(uint32_t buttons, int controller)
 {
 	int i;
+	int *counter = af_counter[controller & 1];
 
 	for (i = 0; i < NCDZ_BUTTON_MAX; i++)
 	{
@@ -71,22 +76,96 @@ static uint32_t update_autofire(uint32_t buttons)
 			{
 				buttons &= ~af_map1[i];
 
-				if (af_counter[i] == 0)
+				if (counter[i] == 0)
 					buttons |= af_map2[i];
 				else
 					buttons &= ~af_map2[i];
 
-				if (++af_counter[i] > af_interval)
-					af_counter[i] = 0;
+				if (++counter[i] > af_interval)
+					counter[i] = 0;
 			}
 			else
 			{
-				af_counter[i] = 0;
+				counter[i] = 0;
 			}
 		}
 	}
 
 	return buttons;
+}
+
+static void set_input_flags(uint32_t buttons)
+{
+	int i;
+
+	for (i = 0; i < MAX_INPUTS; i++)
+		input_flag[i] = (buttons & input_map[i]) != 0;
+}
+
+static void update_inputport_multi(uint32_t controller_count)
+{
+	uint8_t combined_port0 = 0xff;
+	uint8_t combined_port1 = 0xff;
+	uint8_t combined_port2 = 0x0f;
+	uint32_t primary_buttons;
+	uint32_t primary_processed = 0;
+	int saved_controller = option_controller;
+	uint32_t controller;
+
+	if (controller_count > 2)
+		controller_count = 2;
+
+	primary_buttons = poll_gamepad_index(0);
+	if (systembuttons_available ? readHomeButton() :
+	    (primary_buttons & PLATFORM_PAD_START) &&
+	    (primary_buttons & PLATFORM_PAD_SELECT))
+	{
+		showmenu();
+		setup_autofire();
+		primary_buttons = poll_gamepad_index(0);
+	}
+
+	for (controller = 0; controller < controller_count; controller++)
+	{
+		uint32_t buttons = controller == 0 ? primary_buttons :
+			poll_gamepad_index(controller);
+
+		buttons = update_autofire(buttons, (int)controller);
+		set_input_flags(buttons);
+		option_controller = (int)controller;
+
+		update_inputport0();
+		update_inputport1();
+		update_inputport2();
+
+		combined_port0 &= neogeo_port_value[0];
+		combined_port1 &= neogeo_port_value[1];
+		combined_port2 &= neogeo_port_value[2];
+
+		if (controller == 0)
+		{
+			primary_processed = buttons;
+
+			if (input_flag[SNAPSHOT])
+				save_snapshot();
+
+#ifdef COMMAND_LIST
+			if (input_flag[COMMANDLIST])
+				commandlist(1);
+#endif
+		}
+	}
+
+	neogeo_port_value[0] = combined_port0;
+	neogeo_port_value[1] = combined_port1;
+	neogeo_port_value[2] = combined_port2;
+	option_controller = saved_controller;
+	set_input_flags(primary_processed);
+
+	/* Switch Player is intentionally ignored while multiple physical pads are
+	 * active: physical pad N is already bound directly to emulated player N. */
+	if (input_ui_wait > 0)
+		input_ui_wait--;
 }
 
 
@@ -249,6 +328,13 @@ void update_inputport(void)
 {
 	int i;
 	uint32_t buttons;
+	uint32_t controller_count = gamepad_count();
+
+	if (controller_count > 1)
+	{
+		update_inputport_multi(controller_count);
+		return;
+	}
 
 	buttons = poll_gamepad();
 
@@ -267,7 +353,7 @@ void update_inputport(void)
 	}
 #endif
 */
-	buttons = update_autofire(buttons);
+	buttons = update_autofire(buttons, 0);
 
 	for (i = 0; i < MAX_INPUTS; i++)
 		input_flag[i] = (buttons & input_map[i]) != 0;
